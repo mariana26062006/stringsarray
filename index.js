9991fda8135da258fd914fc93bf76808820c91c2
thaisdossena <thaisdossena@gmail.com>
2023-05-03 16:56:28 -0300

    Atividades strings e arrays

---
+++ index.js
@@ -0,0 +1,59 @@
+// let array
+// console.log('a. ', array)
+
+// array = null
+// console.log('b. ', array)
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+//  a - aparecerá indefinido
+// b - aparecerá 'null'
+// c - aparecerá [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// d - aparacerá 3
+// e - aparecerá [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+// f - aparecerá 9
+
+
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+// a resposta será 'subi num ônibus em Mirrocos'
+
+// let nome = prompt("Digite o seu nome: ")
+// let contato = prompt ("Digite o seu e-mail: ")
+// console.log("O e-mail",contato,"foi cadastrado com sucesso. Seja bem-vindo", nome)
+
+// let comidas = ["morango", "banana", "jujuba", "pão", "lasanha"]
+// console.log("Essas são as minhas comidas favoritas: ")
+// console.log(`${comidas[0]}
+// ${comidas[1]}
+// ${comidas[2]}
+// ${comidas[3]}
+// ${comidas[4]}`)
+
+// let usuario = prompt("Digite a sua comida favorita: ");
+// let remove = comidas.splice(1,1, usuario);
+// console.log(comidas)
+
+// let tarefas = []
+// let usuario1 = prompt("Digite a tarefa: ")
+// let usuario2 = prompt("Digite a tarefa: ")
+// let usuario3 = prompt("Digite a tarefa: ")
+
+// console.log(tarefas)
+// let usuario4 = prompt("Informe qual tarefa você já realizou: ")
+// let remove2 = tarefas.splice(usuario4, 1)
+// console.log(tarefas)
+
